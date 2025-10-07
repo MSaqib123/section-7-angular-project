792d37f459a2c7b3957add3ab826c7d4f84f339a
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-07 18:48:17 +0500

    8. Working with Inputs in Custom Directives

--- src/app/_6_to_9/safe-link.directive.ts
+++ src/app/_6_to_9/safe-link.directive.ts
@@ -16,7 +16,43 @@
 //#endregion
 
 //#region  Class #7   7. Using Attribute Directives To Change Element Behavior
-import { Directive, isStandalone } from '@angular/core';
+// import { Directive, isStandalone } from '@angular/core';
+
+// @Directive({
+//   selector: 'a[appSafeLink]',
+//   standalone: true,
+//   host: {
+//     '(click)': 'onConfirmLeavePage($event)',
+//   },
+// })
+// export class SafeLinkDirective {
+//   constructor() {
+//     console.log('Safe Link Directive');
+//   }
+
+//   onConfirmLeavePage(event: MouseEvent) {
+//     // ----- 1st Way -----
+//     // const wantsToLeave = window.confirm("Do you want to leave the app")
+//     // if(wantsToLeave){
+//     //     return
+//     // }
+//     // event?.preventDefault();
+
+//     // ----- 2nd way -----
+//     const wantsToLeave = window.confirm('Do you want to leave the app');
+//     if (wantsToLeave) {
+//       const target = event.currentTarget as HTMLAnchorElement;
+//       if (target && target.href) {
+//         window.open(target.href, '_blank'); // 👈 Opens in a new tab
+//       }
+//     }
+//     event?.preventDefault();
+//   }
+// }
+//#endregion
+
+//#region  Class #8   8. Working with Inputs in Custom Directives
+import { Directive, input, isStandalone } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -26,25 +62,25 @@ import { Directive, isStandalone } from '@angular/core';
   },
 })
 export class SafeLinkDirective {
+    // queryParam = input('myapp');
+    queryParam = input('myapp',{alias:'appSafeLink'});
+
   constructor() {
     console.log('Safe Link Directive');
   }
 
   onConfirmLeavePage(event: MouseEvent) {
-    // ----- 1st Way -----
-    // const wantsToLeave = window.confirm("Do you want to leave the app")
-    // if(wantsToLeave){
-    //     return
-    // }
-    // event?.preventDefault();
-
-    // ----- 2nd way -----
     const wantsToLeave = window.confirm('Do you want to leave the app');
     if (wantsToLeave) {
-      const target = event.currentTarget as HTMLAnchorElement;
-      if (target && target.href) {
-        window.open(target.href, '_blank'); // 👈 Opens in a new tab
-      }
+      //======= 1st way ========
+      //   const address = (event.target as HTMLAnchorElement).href;
+      //   (event.target as HTMLAnchorElement).href = address + "?from=myapp";
+      
+      //======= 1st way ========
+        const address = (event.target as HTMLAnchorElement).href;
+        (event.target as HTMLAnchorElement).href = address + "?from=" + this.queryParam();
+
+      return;
     }
     event?.preventDefault();
   }
