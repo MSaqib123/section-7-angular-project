aacdfe17370ef56fc1506ff59bf58fc967414a71
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-07 16:42:42 +0500

    3. The Starting project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LearningResourcesComponent } from "./_1_to_5/learning-resources/learning-resources.component";
+import { AuthComponent } from "./_1_to_5/auth/auth.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LearningResourcesComponent, AuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
