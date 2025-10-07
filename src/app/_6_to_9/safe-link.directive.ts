5fa304b1f146f7fdaf8a701fb34c5ccc8dece0ef
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-07 18:09:36 +0500

    6. Getting Started with Custom Directives

---
+++ src/app/_6_to_9/safe-link.directive.ts
@@ -0,0 +1,20 @@
+
+
+//#region  Class #6   (6. Getting Started with Custom Directives)
+import { Directive, isStandalone } from "@angular/core";
+
+@Directive({
+    selector:'a[appSafeLink]',
+    standalone:true,
+})
+export class SafeLinkDirective{
+
+    constructor (){
+        console.log("Safe Link Directive");
+        console.log("Safe Link Directive");
+        console.log("Safe Link Directive");
+        console.log("Safe Link Directive");
+    }
+
+}
+//#endregion
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,46 @@
 //=================================================
 //#region 
 
+// import { Component, computed, inject } from '@angular/core';
+// import { LearningResourcesComponent } from "./_1_to_5/learning-resources/learning-resources.component";
+// import { AuthComponent } from "./_1_to_5/auth/auth.component";
+// import { AuthService } from './_1_to_5/auth/auth.service';
+// import { NgFor, NgIf } from '@angular/common';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [ LearningResourcesComponent, AuthComponent, NgIf, NgFor],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   private authService = inject(AuthService);
+
+//   isAdmin = computed(()=> this.authService.activePermission() === 'admin');
+
+//   employees = [
+//     { name: 'Ali', role: 'Developer' },
+//     { name: 'Sara', role: 'Designer' },
+//     { name: 'Ahmed', role: 'Tester' },
+//     { name: 'Zara', role: 'Manager' }
+//   ];
+// }
+
+//#endregion
+
+
+
+
+
+//=================================================
+// ====================== _6_to_9 ==================
+//=================================================
+//#region 
+
 import { Component, computed, inject } from '@angular/core';
-import { LearningResourcesComponent } from "./_1_to_5/learning-resources/learning-resources.component";
-import { AuthComponent } from "./_1_to_5/auth/auth.component";
-import { AuthService } from './_1_to_5/auth/auth.service';
+import { LearningResourcesComponent } from "./_6_to_9/learning-resources/learning-resources.component";
+import { AuthComponent } from "./_6_to_9/auth/auth.component";
+import { AuthService } from './_6_to_9/auth/auth.service';
 import { NgFor, NgIf } from '@angular/common';
 
 @Component({
@@ -20,7 +56,7 @@ export class AppComponent {
 
   isAdmin = computed(()=> this.authService.activePermission() === 'admin');
 
-   employees = [
+  employees = [
     { name: 'Ali', role: 'Developer' },
     { name: 'Sara', role: 'Designer' },
     { name: 'Ahmed', role: 'Tester' },
