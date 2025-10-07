14572c7f1d351284124adb16799f6c92f2aef065
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-07 19:47:48 +0500

    11. Building a Custom Structural Directive

--- src/app/_10_12/auth/auth.directive.ts
+++ src/app/_10_12/auth/auth.directive.ts
@@ -1,4 +1,36 @@
-import { Directive, effect, inject, input } from '@angular/core';
+// #region 10. Building Another Directive
+
+// import { Directive, effect, inject, input } from '@angular/core';
+// import { Permission } from './auth.model';
+// import { AuthService } from './auth.service';
+
+// @Directive({
+//   selector: '[appAuth]'
+// })
+// export class AuthDirective {
+//   userType = input.required<Permission>({alias:'appAuth'});
+//   private  authService = inject(AuthService);
+
+//   constructor() { 
+//     effect(() => {
+//       if(this.authService.activePermission() === this.userType()){
+//         console.log('Show element');
+//       }
+//       else{
+//         console.log("Remove element");
+//       }
+//     })
+//   }
+
+// }
+
+// #endregion
+
+
+
+// #region 11. Building a Custom Structural Directive
+
+import { Directive, effect, inject, input, TemplateRef, ViewContainerRef } from '@angular/core';
 import { Permission } from './auth.model';
 import { AuthService } from './auth.service';
 
@@ -9,15 +41,20 @@ export class AuthDirective {
   userType = input.required<Permission>({alias:'appAuth'});
   private  authService = inject(AuthService);
 
+  private templateRef = inject(TemplateRef);
+  private viewContainerRef = inject(ViewContainerRef);
   constructor() { 
     effect(() => {
       if(this.authService.activePermission() === this.userType()){
-        console.log('Show element');
+        // console.log('Show element');
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
       }
       else{
-        console.log("Remove element");
+        //console.log("Remove element");
+        this.viewContainerRef.clear();
       }
     })
   }
-
 }
+
+// #endregion
