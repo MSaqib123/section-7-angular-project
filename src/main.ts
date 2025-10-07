9cb94891da9300cf3b891cd97da1dfec9aec1adb
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-07 16:16:03 +0500

    3. The Starting Project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+//#region _1_to_5
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/_1_to_5/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+//#endregion
